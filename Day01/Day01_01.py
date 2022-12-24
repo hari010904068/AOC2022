ab76d706805b5021ff85b707d8c94a993279daa1
hari010904068 <hari.haran.mk@gmail.com>
2022-12-24 12:14:54 +0530

    updated solution

--- Day01/Day01_01.py
+++ Day01/Day01_01.py
@@ -1,4 +1,13 @@
 f = open("input.txt")
-print (f.read())
-with open("input.txt", "r") as f:
-    print(max(list(map(sum, [list(map(int, x.split("\n"))) for x in f.read().rstrip("\n").split("\n\n")]))))
+#print (f.read().rstrip("\n").split("\n\n"))
+#print (f.read())
+#solution one liner
+#print('6616\n7349\n7758\n1591\n6068\n9217\n6924\n6766'.split("\n"))
+#l = ['6616', '7349', '7758', '1591', '6068', '9217', '6924', '6766']
+##l1 =(map(int,l))
+# l2 = (list(l1))
+# print(max(l2))
+# print(max(list((map(sum,l2)))))
+# with open("input.txt", "r") as f:
+#     print(max(list(map(sum, [list(map(int, x.split("\n"))) for x in f.read().rstrip("\n").split("\n\n")]))))
+
