059baf49327e38719e4d5ee1f45afd1ff04f1175
hari010904068 <hari.haran.mk@gmail.com>
2022-12-24 12:05:36 +0530

    updated solution courtesy : Reddit solution Megathread

--- Day01/Day01_01.py
+++ Day01/Day01_01.py
@@ -1,2 +1,4 @@
 f = open("input.txt")
 print (f.read())
+with open("input.txt", "r") as f:
+    print(max(list(map(sum, [list(map(int, x.split("\n"))) for x in f.read().rstrip("\n").split("\n\n")]))))
